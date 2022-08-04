7f9abed5480d5f403dfb60d222fb8354d375d04b
HelloAyan <18103373@iubat.edu>
2022-08-05 01:13:54 +0600

    created get method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('HomePage')
+})
+
+app.listen(5000, ()=>{
+    console.log("App is running in port 5000")
+})
