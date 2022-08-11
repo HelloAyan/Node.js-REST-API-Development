09829f766a6d11586dcdb0bafef36fa5d0ac2f16
HelloAyan <18103373@iubat.edu>
2022-08-11 22:51:12 +0600

    installed express

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('HomePage')
+    res.send({
+        msg: 'API is working'
+    })
+})
+
+app.get('/product', (req, res)=>{
+    res.send({
+        name: 'HP Laptop',
+        price: 4000,
+        category: "Electronics",
+        tags: ['electronics', 'monitor', 'hp']
+    })
 })
 
 app.listen(5000, ()=>{
